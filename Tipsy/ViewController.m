a2ada20a8af88f8ebb29e4330ffcfb586e941488
Aayush Mani Phuyal <aayushphuyal@fb.com>
2020-06-23 22:03:42 -0400

    Added MVP Functionality

--- Tipsy/ViewController.m
+++ Tipsy/ViewController.m
@@ -10,6 +10,12 @@
 
 @interface ViewController ()
 
+// Declaring properties for tying up the logic in fields
+@property (weak, nonatomic) IBOutlet UITextField *billField;
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipController;
+
 @end
 
 @implementation ViewController
@@ -19,5 +25,19 @@
     // Do any additional setup after loading the view.
 }
 
+// Making Keyboard go away on tap
+- (IBAction)onTap:(id)sender {
+    [self.view endEditing:YES];
+}
+
+- (IBAction)onEdit:(id)sender {
+    NSArray *tipPercentages = @[@(0.15), @(0.20), @(0.25)];
+    double tipPercentage = [tipPercentages[self.tipController.selectedSegmentIndex] doubleValue];
+    double bill = [self.billField.text doubleValue];
+    double tip = bill * tipPercentage;
+    double total = bill + tip;
+    self.tipLabel.text = [NSString stringWithFormat:@"$%.2f", tip];
+    self.totalLabel.text = [NSString stringWithFormat:@"$%.2f", total];
+}
 
 @end
